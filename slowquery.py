4839b30e638517235d82906001128179ae09b52e
Kamori <kamori.goat@gmail.com>
2013-12-15 22:29:33 -0600

    libraries and debuggin

--- slowquery.py
+++ slowquery.py
@@ -3,7 +3,7 @@
 import os
 import getpass
 import sys
-
+import paramiko
 
 #### Check if slow-query.log exists, if yes remove
 #### Search for database hostname
@@ -16,8 +16,12 @@ import sys
 #### try to do this all without asking for the password 3 times
 
 #### SSH can have an assumed password, unsure if sudo can
-
-
+debug = 0 
+if debug == 1:
+	help('modules')
+	sys.exit()
+else:
+	print('nah')
 
 ## SUDOPASS = getpass.getpass("What is your password:  ")
 
@@ -41,6 +45,21 @@ else:
 
 SUDOPASS = getpass.getpass(WHOAMI + "'s Password: ")
 
+connect = paramiko.SSHClient()
 
+#connect.connect(username=WHOAMI, password=SUDOPASS) 
+
+connect.load_system_host_keys()
+connect.set_missing_host_key_policy(paramiko.WarningPolicy)
+
+connect.connect('166.78.147.249', username=WHOAMI, password=SUDOPASS)
+
+stdin, stdout, stderr = connect.exec_command('ls -al')
+#for line in stdout.readlines():
+#	print(line)
+
+print(stdout.read())
+
+#os.system("ssh " + WHOAMI + "@166.78.147.249" )
 ##print(DATABASE + " will be saved as " + DBSLOW)
 ##print('lets see if we get here')
